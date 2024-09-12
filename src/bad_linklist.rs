7caf5083ac9e69bf0be37548f613e19be49f3c3d
Logickin-Lambda <logickinlambda@protonmail.com>
2024-09-12 11:19:33 +1000

    - Trying to understand how to push a node

--- src/bad_linklist.rs
+++ src/bad_linklist.rs
@@ -16,9 +16,11 @@ enum ListNode{
 }
 
 // We also need a struct to locate the first reference of the list
-struct LinkedList{
+pub struct LinkedList{
     head: ListNode
 }
+
+use std::mem;
 
 impl LinkedList{
     // When we create an initial node, we need to insert a null reference,
@@ -26,6 +28,25 @@ impl LinkedList{
     // This is technically a constructor in other languages.
     pub fn new() -> Self {
         LinkedList{head: ListNode::Nothing}
+    }
+
+    pub fn push(&mut self, input: i32){
+        let content = Box::new(ListContent{
+            element: input,
+            // this is a hack for replace a original memory with a new value
+            // in this case, the nothing node replaces the self.head,
+            // while returning the original un-replaced value from self.head.
+            //
+            // In this example, since we have moved head reference,
+            // suggesting the reference is mutable, so we need to use &mut at input
+            next: mem::replace(&mut self.head, ListNode::Nothing)
+        });
+
+        // Since we have taken the self for other use,
+        // we must giving some back to the original reference.
+        // Rust works like trading which have to return something to the owner,
+        // after taking some stuff from them.
+        self.head = ListNode::Content(content);
     }
 }
 
